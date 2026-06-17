6be1a28cce5c20aa465e3f393611dda56e2f8aac
Yeetmaster7522 <david.f.santillan@gmail.com>
2026-06-17 19:11:56 +1000

    made validation into a class

--- validation.py
+++ validation.py
@@ -2,45 +2,56 @@ from torch import load, device, accelerator, no_grad
 from torch import max as tmax
 
 from models import ocr_v1 as model
-from dataloader import TrainTestLoader as ttl
-from dataloader import CHARSET
+from dataloader import TrainTestLoader, CHARSET
 
-PATH = "models/models_F/model_1_F_3.pt"
-DEVICE = device(accelerator.current_accelerator().type if accelerator.is_available() else 'cpu')
+class Validator:
+    def __init__(self, path, model, root_dir):
+        self.path = path
+        self.model = model
+        self.root_dir = root_dir
+
+        self.device = device(accelerator.current_accelerator().type if accelerator.is_available() else 'cpu')
+        print(f"Using device: {self.device}")
+
+    def check_accuracy(self):
+        net = self.model().to(self.device)
+        net.load_state_dict(load(self.path, weights_only=True))
+
+        tl = TrainTestLoader(root_dir=self.root_dir).trainloader
+
+        correct = 0
+        total = 0
+
+        class_correct = {classname: 0 for classname in CHARSET}
+        class_total = {classname: 0 for classname in CHARSET}
+
+        with no_grad():
+            print("Validation started")
+
+            for images, labels in tl:
+                images, labels = images.to(self.device), labels.to(self.device)
+
+                outputs = net(images)
+                _, predicted = tmax(outputs, 1)
+
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+                for label, prediction in zip(labels, predicted):
+                    if label == prediction:
+                        class_correct[CHARSET[int(label)]] += 1
+                    class_total[CHARSET[int(label)]] += 1
+
+        print(f"Accuracy [{total}]: {100 * correct / total:.3f}%")
+
+        for classname, correct_count in class_correct.items():
+            accuracy = 100 * float(correct_count) / class_total[classname]
+            print(f"Accuracy for class: {classname} is {accuracy:.3f}%")
 
 if __name__ == "__main__":
-    net = model().to(DEVICE)
-    net.load_state_dict(load(PATH, weights_only=True))
-
-    tl = ttl(root_dir="dataset/character_images_no_noise").trainloader
-
-    print(f"Using device: {DEVICE}")
-
-    correct = 0
-    total = 0
-
-    class_correct = {classname: 0 for classname in CHARSET}
-    class_total = {classname: 0 for classname in CHARSET}
-
-    with no_grad():
-        print("Validation started")
-
-        for images, labels in tl:
-            images, labels = images.to(DEVICE), labels.to(DEVICE)
-
-            outputs = net(images)
-            _, predicted = tmax(outputs, 1)
-
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-            for label, prediction in zip(labels, predicted):
-                if label == prediction:
-                    class_correct[CHARSET[int(label)]] += 1
-                class_total[CHARSET[int(label)]] += 1
-
-    print(f"Accuracy [{total}]: {100 * correct / total:.3f}%")
-
-    for classname, correct_count in class_correct.items():
-        accuracy = 100 * float(correct_count) / class_total[classname]
-        print(f"Accuracy for class: {classname} is {accuracy:.3f}%")
+    validator = Validator(
+        path="models/models_F/model_1_F_3.pt",
+        model=model,
+        root_dir="dataset/character_images_no_noise"
+    )
+    validator.check_accuracy()
