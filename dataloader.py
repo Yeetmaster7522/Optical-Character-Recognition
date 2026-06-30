106ab2aceb05a6ca2e82e89367555eb51e2b2f96
Yeetmaster7522 <david.f.santillan@gmail.com>
2026-06-30 18:57:10 +1000

    updated dataloader to include parent and child class structure

--- dataloader.py
+++ dataloader.py
@@ -12,7 +12,11 @@ import numpy as np
 import os
 import warnings
 
+
+
 warnings.filterwarnings("ignore", category=UserWarning, module="torch.utils.data.dataloader")
+
+
 
 """
 CHARSET and char_to_idx written by Copilot.
@@ -28,17 +32,16 @@ CHARSET = (
     [chr(i) for i in range(48, 58)] +      # 0–9
     ['@', '#', '$', '%', '&', '+', '?', '<', '>']
 )
+
 char_to_idx = {c: i for i, c in enumerate(CHARSET)}
+
+
+
 
 class CharacterDataset(Dataset):
     """
     Character img dataset.
     https://docs.pytorch.org/tutorials/beginner/data_loading_tutorial.html
-
-    Attributes:
-        root_dir: A filepath to a folder with images
-        images: Filename and label of each image in root_dir
-        transform: The transform applied to an image
     """
     
     def __init__(self, root_dir: str, transform=None):
@@ -55,19 +58,21 @@ class CharacterDataset(Dataset):
             raise Exception(f"Root directory: {root_dir} does not exist")
 
         # Set class attributes
-        self.root_dir = root_dir
-        self.transform = transform
+        self.__root_dir = root_dir
+        self.__transform = transform
 
         # Loops through each filepath in the root_dir and saves the filename and label.
-        self.images = [{
+        self.__images = [{
             "filename": filepath, 
             "label": char_to_idx[chr(int(filepath.removesuffix(".png").split("_")[2]))]
             } for filepath in os.listdir(root_dir)]
 
+
     def __len__(self):
         """Returns amount of items in self.images"""
-        return len(self.images)
-    
+        return len(self.__images)
+
+
     def __getitem__(self, idx):
         """
         Returns grayscale image and label in format:
@@ -79,19 +84,64 @@ class CharacterDataset(Dataset):
             idx = idx.tolist()
 
         # Joins together root directory and filename to get filepath
-        filepath = os.path.join(self.root_dir, self.images[idx]["filename"])
+        filepath = os.path.join(self.__root_dir, self.__images[idx]["filename"])
 
         # Opens filepath as an image
         with Image.open(filepath) as image:
             image = image.convert("L") # Makes image grayscale
 
             # Applies transform to image if applicable
-            if self.transform:
-                image = self.transform(image)
+            if self.__transform:
+                image = self.__transform(image)
 
-        return image, self.images[idx]["label"]
+        return image, self.__images[idx]["label"]
     
-class TrainTestLoader:
+    @property
+    def images(self):
+        return self.__images
+
+class BaseLoader:
+    """
+    Base for utility classes for CharacterDataset to provide efficient data loading.
+    """
+
+    def __init__(self, root_dir: str, seed):
+        """
+        Initialise Loader.
+
+        Keyword arguments:
+            root_dir: Filepath where dataset is stored
+        
+        Optional keyword arguments:
+            seed: Starting point for RNG
+            split: Fraction representing how much of the dataset is split into the trainset
+            batch_size: Number of data samples processed in a single iteration
+        """
+
+        # Transform for images. Converst PIL image, numpy array, or tensor into Image tensor 
+        # and normalises pixel values to be from 0 to 1.
+        transform = v2.Compose([
+            v2.ToImage(),
+            v2.ToDtype(float32, scale=True)
+        ])
+
+        # Create Generator object to manage RNG
+        self.__generator = Generator().manual_seed(seed)
+
+
+        # Create dataset and split it into train and test sets
+        self.__dataset = CharacterDataset(root_dir=root_dir, transform=transform)
+
+
+    @property
+    def dataset(self):
+        return self.__dataset
+    
+    @property
+    def batch_size(self):
+        return self.__batch_size
+
+class TrainTestLoader(BaseLoader):
     """
     Utility class for CharacterDataset to provide efficent data loading.
 
@@ -117,36 +167,66 @@ class TrainTestLoader:
             split: Fraction representing how much of the dataset is split into the trainset
             batch_size: Number of data samples processed in a single iteration
         """
-        
-        # Transform for images. Converst PIL image, numpy array, or tensor into Image tensor 
-        # and normalises pixel values to be from 0 to 1.
-        transform = v2.Compose([
-            v2.ToImage(),
-            v2.ToDtype(float32, scale=True)
-        ])
 
-        # Create Generator object to manage RNG
-        generator = Generator().manual_seed(seed)
+        super().__init__(root_dir, seed)
+        self.__trainset, self.__testset = random_split(
+            self.dataset, 
+            [1-split, split], 
+            generator=self.generator
+            )
 
-        # Create dataset and split it into train and test sets
-        self.dataset = CharacterDataset(root_dir=root_dir, transform=transform)
-        self.trainset, self.testset = random_split(self.dataset, [1-split, split], generator=generator)
 
         # Create dataloaders
-        if split != 1:
-            self.trainloader = DataLoader(self.trainset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-        else:
-            self.trainloader = []
-        
-        self.testloader = DataLoader(self.testset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+        self.__trainloader = DataLoader(
+            self.__trainset, 
+            batch_size=batch_size, 
+            shuffle=True, 
+            num_workers=4, 
+            pin_memory=True
+            )        
+        self.__testloader = DataLoader(
+            self.__testset, 
+            batch_size=batch_size, 
+            shuffle=False, 
+            num_workers=4, 
+            pin_memory=True
+            )
+
 
         # Batch size reference for other programs
-        self.batch_size = batch_size
+        self.__batch_size = batch_size
 
-        self.train_totalbatches = len(self.trainloader)
-        self.test_totalbatches = len(self.testloader)
+        self.__train_totalbatches = len(self.__trainloader)
+        self.__test_totalbatches = len(self.__testloader)
 
-class FullLoader:
+
+    @property
+    def trainset(self):
+        return self.__trainset
+    
+    @property
+    def testset(self):
+        return self.__testset
+    
+    @property
+    def trainloader(self):
+        return self.__trainloader
+    
+    @property
+    def testloader(self):
+        return self.__testloader
+
+    @property
+    def train_totalbatches(self):
+        return self.__train_totalbatches
+    
+    @property
+    def test_totalbatches(self):
+        return self.__test_totalbatches
+
+
+
+class FullLoader(BaseLoader):
     """
     Utility class for CharacterDataset to provide efficent data loading.
 
@@ -157,7 +237,7 @@ class FullLoader:
         batch_size: Number of data samples processed in a single iteration
         totalbatches: Number of batches within loader
     """
-    def __init__(self, root_dir: str, batch_size=256):
+    def __init__(self, root_dir: str, batch_size=256, seed=42):
         """
         Initialise TrainTestLoader.
 
@@ -168,22 +248,31 @@ class FullLoader:
             batch_size: Number of data samples processed in a single iteration
         """
         
-        # Transform for images. Converst PIL image, numpy array, or tensor into Image tensor 
-        # and normalises pixel values to be from 0 to 1.
-        transform = v2.Compose([
-            v2.ToImage(),
-            v2.ToDtype(float32, scale=True)
-        ])
+        super().__init__(root_dir, seed)
+        self.__loader = DataLoader(
+            self.dataset, 
+            batch_size=batch_size, 
+            shuffle=False, 
+            num_workers=4, 
+            pin_memory=True
+            )
 
-        # Create dataset and then directly convert it into dataloader
-        self.dataset = CharacterDataset(root_dir=root_dir, transform=transform)
-        trainset, testset = random_split(self.dataset, [0, 1])
-        self.loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
 
         # Batch size reference for other programs
-        self.batch_size = batch_size
+        self.__batch_size = batch_size
 
-        self.totalbatches = len(self.loader)
+        self.__totalbatches = len(self.__loader)
+
+
+    @property
+    def loader(self):
+        return self.__loader
+
+    @property
+    def totalbatches(self):
+        return self.__totalbatches
+
+
 
 def imshow(img: tv_tensors.Image):
     """
@@ -201,8 +290,8 @@ def imshow(img: tv_tensors.Image):
 if __name__ == "__main__":
     ttl = TrainTestLoader(root_dir="character_images_no_noise")
 
-    print(f"Amount of items in dataset: {len(ttl.dataset)}")
-    dataiter = iter(ttl.trainloader)
+    print(f"Amount of items in dataset: {len(ttl.__dataset)}")
+    dataiter = iter(ttl.__trainloader)
     images, labels = next(dataiter)
     imshow(make_grid(images))
-    print(" ".join(f"{labels[j].item():5}" for j in range(ttl.batch_size)))
+    print(" ".join(f"{labels[j].item():5}" for j in range(ttl.__batch_size)))
